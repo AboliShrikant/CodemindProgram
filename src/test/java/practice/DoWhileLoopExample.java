cee63a7082696f8d36fff422f80a58fc431179bf
aboli <jadhavaboli123@gmail.com>
2024-06-08 13:55:16 +0530

    do while example

---
+++ src/test/java/practice/DoWhileLoopExample.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class DoWhileLoopExample 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=10;
+		
+		do {
+			
+			System.out.println("Value of i : " + i);
+			
+			i--;
+			
+		} while(i>=1);
+		
+	}
+
+
+
+}
