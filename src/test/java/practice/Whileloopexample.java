1140e8fe23b6a22b024d780c5cb795e2c2c3c57e
aboli <jadhavaboli123@gmail.com>
2024-06-08 17:41:26 +0530

    whileloopexample done

---
+++ src/test/java/practice/Whileloopexample.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Whileloopexample {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i = 1;
+		
+		while (i<=10) { // True/False
+			
+			
+			System.out.println("Value of i : " + i);
+			
+			i++;
+		}
+		
+	}
+
+	
+
+}
